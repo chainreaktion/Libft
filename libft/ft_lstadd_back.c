514e3d2b53f58bd711da9a49f061d903bfb0f5d9
Johanna Schmitz <jschmitz@student.42.fr>
2024-05-28 16:28:25 +0200

    start bonus early

---
+++ libft/ft_lstadd_back.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jschmitz <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/28 15:59:51 by jschmitz          #+#    #+#             */
+/*   Updated: 2024/05/28 16:20:46 by jschmitz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*back;
+	 //check first that lst and new are no nullpointers
+	if (lst == NULL)
+		return ;
+	//case if the list that the pointer points to is empty
+	if (*lst == NULL)
+	{
+		*lst = new;
+		return ;
+	}
+	last = ft_lstlast(*lst);
+	last->next = new;
+	new->next = NULL;
+	//not sure if the last step is relly necessary
+}
